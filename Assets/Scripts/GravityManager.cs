15c4680e3b05cea3c4b39331a73928f34e8025f5
Alejandro  Arevalo Martinez <alex10s.k.s@gmail.com>
2017-07-26 18:26:48 +0200

    script not working

--- Assets/Scripts/GravityManager.cs
+++ Assets/Scripts/GravityManager.cs
@@ -1,64 +1,68 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class GravityManager : MonoBehaviour {
-    private Vector3 forceToAdd = Vector3 (5, 5, 0);
-    private GameObject obj;
-    private RotateObject other ;
-    private Boolean rotating = false;
-    private Boolean changegravityminus = false;
-    private Boolean changegravityplus = false;
+public class GravityTest : MonoBehaviour
+{
+    public float gravity = 100.0f;
+    public float cutoffDistance = 10.0f;
+    public Rigidbody planet;
+    public Rigidbody player;
 
-    function Start () {
-        other = obj.GetComponent (RotateObject);
+    private Vector3[] m_GravityDirections =
+    {
+         Vector3.up,
+         Vector3.down,
+         Vector3.left,
+         Vector3.right,
+         Vector3.forward,
+         Vector3.back
+     };
+
+    private float UniversalGravitation(Rigidbody a, Rigidbody b)
+    {
+        float distance = Vector3.Distance(a.transform.position, b.transform.position);
+        if (distance >= cutoffDistance)
+            return 0.0f;
+
+        return gravity * (a.mass * b.mass) / distance;
     }
 
-    function Update () {
-        if (other.gravity == 1) {
-            transform.Rotate (0, 0, 0);
-            rigidbody.AddForce (0, 0, 0);
-            Physics.gravity = new
-            Vector3 (0f, -9.81f, 0f);
+    private Vector3 CubeGravityDirection(Rigidbody planet, Rigidbody other)
+    {
+        float bestDot = -1.0f;
+        Vector3 direction = Vector3.zero;
+
+        for (int index = 0; index < m_GravityDirections.Length; index++)
+        {
+            Vector3 gravityDirection = planet.transform.TransformVector(m_GravityDirections[index]).normalized;
+            Vector3 directionToOther = (other.transform.position - planet.transform.position).normalized;
+
+            float dot = Vector3.Dot(gravityDirection, directionToOther);
+
+            if (dot > bestDot)
+            {
+                direction = gravityDirection;
+                bestDot = dot;
+            }
         }
 
-        if (other.gravity == 2) {
-            transform.Rotate (0, 0, 90);
-            rigidbody.AddForce (0, 0, 0);
-            Physics.gravity = new
-            Vector3 (-9.81f, 0f, 0f);
-        }
+        return direction;
+    }
 
-        if (other.gravity == 3) {
-            transform.Rotate (0, 0, 90);
-            rigidbody.AddForce (0, 0, 0);
-            Physics.gravity = new
-            Vector3 (0f, 9.81f, 0f);
-        }
+    private Vector3 CubeGravityForce(Rigidbody planet, Rigidbody other)
+    {
+        float gravity = UniversalGravitation(planet, other);
+        Vector3 direction = CubeGravityDirection(planet, other);
 
-        if (other.gravity == 4) {
-            transform.Rotate (0, 0, 90);
-            rigidbody.AddForce (0, 0, 0);
-            Physics.gravity = new
-            Vector3 (9.81f, 0f, 0f);
-        }
+        return gravity * direction * -1.0f;
+    }
 
-        if (Input.GetKeyDown (KeyCode.PageUp) && rotating == false) {
-            changegravityminus = false;
-            rotating = true;
-            transform.Rotate (0, 0, -90);
-            rigidbody.velocity = forceToAdd;
-            Physics.gravity = Quaternion.Euler (0, 0, -90) * Physics.gravity;
-            rotating = false;
-            changegravityminus = true;
-        }
+    public void FixedUpdate()
+    {
+        Vector3 gravityForce = CubeGravityForce(planet, player);
+        player.AddForce(gravityForce);
 
-        if (Input.GetKeyDown (KeyCode.PageDown) && rotating == false) {
-            rotating = true;
-            transform.Rotate (0, 0, 90);
-            rigidbody.velocity = forceToAdd;
-            Physics.gravity = Quaternion.Euler (0, 0, 90) * Physics.gravity;
-            rotating = false;
-        }
+        Vector3 up = CubeGravityDirection(planet, player);
+        player.transform.rotation = Quaternion.LookRotation(player.transform.forward, up);
     }
 }
